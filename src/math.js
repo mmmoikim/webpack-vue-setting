a8da47fd6bff617ceae82422b47a0d7f10b6a6f6
mmmoikim <mmmoikim@gmail.com>
2019-01-30 11:14:41 +0900

    vue and html generate plugin

--- src/math.js
+++
@@ -1,7 +0,0 @@
-export function square(n) {
-    return n * n;
-}
-
-export function cube(x) {
-    return x * x * x;
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,89 @@
 const path = require('path');
 const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env, options) => {
-  const config = {
-    entry: {
-      app: ['./src/index.js']
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-      rules: [{
-        test: /\.js/,
-        exclude: /(node_modules)/,
-        use: [{
-          loader: 'babel-loader'
-        }]
-      }]
-    },
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all'
-          }
-        }
-      }
+    const config = {
+        entry: {
+            app: ['./src/main.js']
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [{
+                    test: /\.js/,
+                    exclude: /(node_modules)/,
+                    use: [{
+                        loader: 'babel-loader'
+                    }]
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'vue-style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader'
+                }
+            ]
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all'
+                    }
+                }
+            }
+        },
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js'
+            },
+            extensions: ['*', '.js', '.vue', '.json']
+        },
     }
-  }
 
-  if (options.mode === 'development') {
-    config.plugins = [
-      new webpack.HotModuleReplacementPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'Development',
-        showErrors: true
-      }),
-      // new BundleAnalyzerPlugin()
-    ];
-
-    config.devtool = 'inline-source-map';
-
-    config.devServer = {
-      hot: true,
-      host: 'localhost',
-      contentBase: path.resolve(__dirname, 'dist'),
-      stats: {
-        color: true
-      }
-    };
-  } else {
-    config.plugins = [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin()
-    ];
-  }
-  return config;
+    if (options.mode === 'development') {
+        config.devtool = 'inline-source-map';
+        config.devServer = {
+            hot: true,
+            contentBase: path.resolve(__dirname, 'dist'),
+            allowedHosts: ['host.com'],
+            historyApiFallback: true,
+            overlay: true
+        };
+        config.plugins = [
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                showErrors: true,
+                template: 'src/index.html'
+            }),
+            new VueLoaderPlugin()
+            // new BundleAnalyzerPlugin()
+        ];
+    } else {
+        config.plugins = [
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebpackPlugin({
+                template: 'src/index.html'
+            }),
+            new VueLoaderPlugin()
+        ];
+    }
+    return config;
 }
